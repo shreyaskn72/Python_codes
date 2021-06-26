df6bdd42c5e2572e6e3063ed478375f8c4e33ecc
shreyaskn72 <60369972+shreyaskn72@users.noreply.github.com>
2021-06-26 14:06:55 +0530

    Add files via upload

---
+++ database/sqlite-in-python/4-read_resource/read_resource.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+with sqlite3.connect("university.db") as con:
+    cur = con.cursor()
+    print("Database created")
+    cur.execute("""CREATE TABLE IF NOT EXISTS students (
+                first text,
+                last text,
+                roll_no text,
+                rank integer
+                )""")
+    print("table created")
+
+    cur.execute('''INSERT INTO students(
+       first, last, roll_no, rank) VALUES 
+       ('Shreyas', 'KN72', '20FECD8799', 1)''')
+
+    print("resource added")
+
+    cur.execute('''INSERT INTO students(
+           first, last, roll_no, rank) VALUES 
+           ('parvin', 'jolly', '80gty96', 10)''')
+
+    print("resource added")
+
+    # Readoing specific records using the where clause
+    cur.execute("SELECT * from students WHERE last == 'KN72'")
+    print(cur.fetchall())
+
+    con.commit()
+
+
+con.close()
