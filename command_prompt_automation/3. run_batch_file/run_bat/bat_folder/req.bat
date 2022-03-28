477bf47cbfdea5302107d9ea70107f1af5613c0e
shreyaskn72 <60369972+shreyaskn72@users.noreply.github.com>
2022-03-28 16:11:39 +0530

    Add files via upload

---
+++ command_prompt_automation/3. run_batch_file/run_bat/bat_folder/req.bat
@@ -0,0 +1,5 @@
+cd C:/Users/shreyas/PythonProjects/command_prompt_automation/venv
+CALL Scripts\activate
+
+cd C:/Users/shreyas/PythonProjects/command_prompt_automation/0.requirements_folder
+pip3 install -r requirements.txt
