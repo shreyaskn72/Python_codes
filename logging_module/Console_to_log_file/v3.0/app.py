c20651a8678708b93f6028bc60f99d9c2a58c39c
shreyaskn72 <60369972+shreyaskn72@users.noreply.github.com>
2023-02-08 19:57:43 +0530

    Add files via upload

---
+++ logging_module/Console_to_log_file/v3.0/app.py
@@ -0,0 +1,9 @@
+#Reference:
+
+
+from module1.helloword import hello
+
+if __name__ == "__main__":
+    #path = r"C:\Users\shreyaskn72\Downloads\message5.log"
+    path = "message.log"
+    hello(path,"hi", "Shreyas")
