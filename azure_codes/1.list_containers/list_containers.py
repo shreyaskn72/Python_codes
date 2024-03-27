11fe2a6c91d47b67ab93beaeb6c3bfab22f0f024
shreyaskn72 <shreyaskn72@gmail.com>
2024-03-27 19:57:37 +0530

    list_azure containers

--- azure_codes/1.list_containers/list_containers.py
+++ azure_codes/1.list_containers/list_containers.py
@@ -11,12 +11,19 @@ def list_containers():
         # List containers
         containers = blob_service_client.list_containers()
 
+        containers_name = []
+
         print("List of containers:")
         for container in containers:
             print(container.name)
+            containers_name.append(container.name)
+
+        return containers, containers_name
 
     except Exception as e:
         print("An error occurred:", e)
 
 if __name__ == "__main__":
-    list_containers()
+    containers, containers_name = list_containers()
+    print("containers", containers)
+    print("containers_name", containers_name)
